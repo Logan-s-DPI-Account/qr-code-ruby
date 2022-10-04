5ad87ce3adf787bee8ecedc346b002d393b16e18
Logan CoBell <lcobell@cps.edu>
2022-10-04 19:13:25 +0000

    QR Codes work

--- qr.rb
+++ qr.rb
@@ -1,1 +1,9 @@
-p "Hello World"
+require "rqrcode"
+
+# qrcode = RQRCode:: QRCode.new("www.logancobell.com")
+# qrcode = RQRCode:: QRCode.new("WIFI:T:WPA;S:My wifi DoodleNet;P:DoodleNet2022$;;")
+qrcode = RQRCode:: QRCode.new("SMSTO:+17576927038:Hi")
+
+png = qrcode.as_png({ :size => 500 })
+
+IO.binwrite("wifi.png", png.to_s)
